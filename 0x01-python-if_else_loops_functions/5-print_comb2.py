9b2211504ac75dbc2ebb6aa7c38b8d5006564677
AnitaMatt <arneethar16@yahoo.com>
2022-05-31 07:14:31 -0700

    printing a comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,8 +2,6 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i == 9 and j == 9:
-            print("{:d}".format(i), end="")
-            print("{:d}".format(j))
-            break
-        print("{:d}".format(i), end="")
-        print("{:d}".format(j), end=", ")
+            print("{:d}{:d}".format(i,j))
+        else:
+            print("{:d}{:d}".format(i,j), end=", ")
