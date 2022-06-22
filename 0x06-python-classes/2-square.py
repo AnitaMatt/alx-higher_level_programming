689986aa4c4c2465b43bfd26f4d428871794502a
AnitaMatt <arneethar16@yahoo.com>
2022-06-21 19:50:55 -0700

    error handling in a class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a class square"""
+
+
+class Square:
+    """Defining a square"""
+    def __init__(self, size=0):
+        """Initiazing a square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
