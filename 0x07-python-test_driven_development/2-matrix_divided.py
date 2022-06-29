f7ecabde3757aaee3a98cd22dc32246b230d2da6
AnitaMatt <arneethar16@yahoo.com>
2022-06-29 09:04:36 -0700

    corrected some pystyling

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Function that divides all elements of a matrix"""
+
+
 def matrix_divided(matrix, div):
     """ checking that the matrix is a list of list and other errors"""
     if not isinstance(matrix, list):
@@ -7,12 +9,13 @@ def matrix_divided(matrix, div):
                         " of integers/floats")
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) "
+                            "of integers/floats")
         for ele in row:
-            if not isinstance(ele, (int,float)):
+            if not isinstance(ele, (int,  float)):
                 raise TypeError("matrix must be a matrix (list of lists)"
                                 " of integers/floats")
-    if not all(len(i) == len(matrix[0])  for i in matrix):
+    if not all(len(i) == len(matrix[0]) for i in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
