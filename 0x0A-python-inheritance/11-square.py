f215893eb2f3c5470a352931186b2aeefcf4aa05
AnitaMatt <arneethar16@yahoo.com>
2022-07-07 15:07:27 -0700

    properly initializing the names

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,4 +6,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """defing a square as a subclass of rectangle based on 9-rectangle.py"""
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
