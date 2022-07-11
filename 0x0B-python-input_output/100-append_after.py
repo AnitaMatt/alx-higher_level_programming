cced9ed6171d7f19181c7275a4310b922a5a9fff
AnitaMatt <arneethar16@yahoo.com>
2022-07-11 08:09:25 -0700

    check the line not the new_content

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,7 +10,7 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, encoding="utf-8") as fd:
         for line in fd:
             new_content += line
-            if search_string in new_content:
+            if search_string in line:
                 new_content += new_string
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(new_content)
