cf7af8e3abb611c97b15cb0f2ffee0cafa53af01
AnitaMatt <arneethar16@yahoo.com>
2022-07-11 08:04:22 -0700

    appending to a file after a word

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that inserts a line of text to a file
+   after each line containing a specific string
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append new string after each search string"""
+    new_content = ""
+    with open(filename, encoding="utf-8") as fd:
+        for line in fd:
+            new_content += line
+            if search_string in new_content:
+                new_content += new_string
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(new_content)
