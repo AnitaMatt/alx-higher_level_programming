66ae4e46c1ee0198ad237a2f72a2bd0552c1e4f0
AnitaMatt <arneethar16@yahoo.com>
2022-07-11 06:43:21 -0700

    replacing a json within a class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""defines a class student"""
+
+
+class Student:
+    """ A class with one public method and
+        three public instance attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        """initializing with three attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of
+           Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            j = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    j.update({i: getattr(self, i)})
+            return j
+
+    def reload_from_json(self, json):
+        """ replace the json with new attibutes"""
+        for i, j in json.items():
+            setattr(self, i, j)
