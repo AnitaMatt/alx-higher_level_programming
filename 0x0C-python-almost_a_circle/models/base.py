8367fa72418c68f0542fa529043aea42672c1625
AnitaMatt <arneethar16@yahoo.com>
2022-07-16 09:46:57 -0700

    saving and loading from a csv file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This file will contain a base class"""
 import json
+import csv
 
 
 class Base:
@@ -66,3 +67,37 @@ class Base:
                 for r in list_objs:
                     dict_list.append(r.to_dictionary())
                 fd.write(cls.to_json_string(dict_list))
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """saves to a csv file"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ['id', 'width', 'height', 'x', 'y']
+                else:
+                    fieldnames = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """returns a list of instances of a csv file"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, encoding="utf-8", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ['id', 'width', 'height', 'x', 'y']
+                else:
+                    fieldnames = ['id', 'size', 'x', 'y']
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
