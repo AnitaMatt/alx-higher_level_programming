1bf55889568c008564047f6d6157bd909cb73fd7
AnitaMatt <arneethar16@yahoo.com>
2022-07-24 06:55:13 -0700

    private attributes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     """Defining a square"""
     def __init__(self, size=0, position=(0, 0)):
         """Initiazies a square class"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
