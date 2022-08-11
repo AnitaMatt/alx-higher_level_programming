26444735adfb3acfa7257dff614442ebe9de16d0
AnitaMatt <arneethar16@yahoo.com>
2022-08-11 07:00:51 -0700

    corrected mistake

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ FROM tv_genres
 RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
-ORDER BY COUNT(tv_genres.id) DESC
+ORDER BY COUNT(tv_show_genres.genre_id) DESC
