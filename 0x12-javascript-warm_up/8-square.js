f4c7ca2ea82797340fdcd5116972fa8fc7eaec62
AnitaMatt <arneethar16@yahoo.com>
2022-09-05 07:24:16 -0700

    printing a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,12 +2,12 @@
 const args = Math.floor(parseInt(process.argv[2]));
 if (!args) {
   console.log('Missing size');
-}else {
-    for (let i = 0; i < args; i++) {
-	let result = ""
-      for (let j = 0; j < args; j++) {
-	  result += 'X';
-      }
-	console.log(result);
+} else {
+  for (let i = 0; i < args; i++) {
+    let result = '';
+    for (let j = 0; j < args; j++) {
+      result += 'X';
+    }
+    console.log(result);
   }
 }
