a306ce0279adb15b19da1deeacd22c54f04843aa
AnitaMatt <arneethar16@yahoo.com>
2022-09-05 06:08:16 -0700

    Fix: correct output for no arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const numOfArgv = process.argv.length - 2;
 if (numOfArgv === 0) {
-  console.log('No argument found');
+  console.log('No argument');
 } else if (numOfArgv === 1) {
   console.log('Argument found');
 } else {
