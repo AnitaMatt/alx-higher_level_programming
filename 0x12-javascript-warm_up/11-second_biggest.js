351ebe0c2725e4896a433cc6d3bd488e23c32a26
AnitaMatt <arneethar16@yahoo.com>
2022-09-06 03:28:50 -0700

    Fix: second largest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const args = process.argv;
 let largest = parseInt(args[2]);
-let second = largest;
 if (args.length < 4) {
   console.log(0);
 } else {
+  let second = largest;
   for (let i = 2; i < args.length; i++) {
     if (args[i] > largest) {
       second = largest;
       largest = parseInt(args[i]);
+    } else {
+      second = parseInt(args[i]);
     }
   }
   console.log(second);
