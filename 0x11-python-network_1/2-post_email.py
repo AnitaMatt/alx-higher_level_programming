093f38b9c28640564bd196e7305895e759b8c637
AnitaMatt <arneethar16@yahoo.com>
2022-10-27 07:46:18 -0700

    posting an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""post an email"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+value = {'email':  sys.argv[2]}
+
+data = urllib.parse.urlencode(value)
+
+data = data.encode('ascii')
+
+req = urllib.request.Request(sys.argv[1], data)
+
+with urllib.request.urlopen(req) as res:
+    print(res.read().decode('utf-8'))
