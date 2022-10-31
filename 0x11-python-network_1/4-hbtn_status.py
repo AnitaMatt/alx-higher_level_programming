82e72dc7417fe010d769e6eac541d3055ddfb413
AnitaMatt <arneethar16@yahoo.com>
2022-10-31 04:40:56 -0700

    using request package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""using request package"""
+
+import requests
+
+res = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print("\t type: {}".format(type(res.text)))
+print("\t content: {}".format(res.text))
