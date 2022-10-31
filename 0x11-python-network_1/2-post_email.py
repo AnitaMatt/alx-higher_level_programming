e968a6fa88dba70fd32ac98077b4b32b5c9d0951
AnitaMatt <arneethar16@yahoo.com>
2022-10-31 04:00:11 -0700

    merge fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 
-""" post an email"""
-=======
 """make a post request """
->>>>>>> 82ebe6649c3f3440a2d83fc2ea646aebd23a7b19
 
 import urllib.request
 import urllib.parse
