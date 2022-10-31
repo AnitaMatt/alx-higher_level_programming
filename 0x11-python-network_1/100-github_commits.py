0310b56a4afa8bbd701cea6637d36fff7e44b3c3
AnitaMatt <arneethar16@yahoo.com>
2022-10-31 06:25:31 -0700

    style: pep8

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -16,11 +16,11 @@ commits'.format(owner, repo)
     r = requests.get(url)
 
     r = r.json()
-    
 
     try:
         for i in range(10):
             print('{}: {}'.format(r[i].get('sha'),
-                              r[i].get('commit').get('author').get('name')))
+                                  r[i].get('commit')
+                                  .get('author').get('name')))
     except IndexError:
         pass
